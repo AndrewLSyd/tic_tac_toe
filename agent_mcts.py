8c630724dea03ce173db40d5845c4cc03e04aa16
Andrew Lau <andrew.ondu.lau@gmail.com>
2019-04-20 15:42:50 +1000

    3:42

--- agent_mcts.py
+++ agent_mcts.py
@@ -20,6 +20,11 @@ class MCTSNode:
         self._moves_not_tried = state.get_moves()
         self._prev_player = state.get_prev_player()
 
+    def __repr__(self):
+        out = "{Move - " + str(self._move) + " Wins/Visits:" + str(self._wins) + "/"\
+                + str(self._visits) + " UCT:" + str(self._moves_not_tried) + "}"
+        return out
+
     def get_wins(self):
         """ returns number of wins """
         return self._wins
@@ -48,21 +53,26 @@ class MCTSNode:
         """ returns previous player """
         return self._prev_player
 
+    def update(self, result):
+        """ update node """
+        self._wins = self._wins + result
+        self._visits = self._visits + 1
+
     def choose_child_uct(self, exploration=math.sqrt(2)):
         """ We use the Upper Confidence Bound for Trees formula to select nodes to expand.
-        We can use this to balance exploration vs exploitation. """
-        # we can tune the exploration parameter here
+        Use the exploration parameter to balance exploitation and exploration. """
+        # get the most promising child by selecting the child with the highest UCT value
         chosen_child = sorted(self._children,
+                              # lambda function that calculates the value based on UCT
                               key=lambda child: child.get_wins() / child.get_visits()
                               + exploration * math.sqrt(math.log(self.get_visits())
-                                          / child.get_visits()))[-1]
-        # get the most promising child by taking the one with the highest value
+                                                        / child.get_visits()))[-1]
         return chosen_child
 
     def child_add(self, move_tried, state):
         """ get rid of tried move from moves not tried and return added child. """
         # set node
-        node = MCTSNode(move=move_tried, parent=self, state=state)
+        node = MCTSNode(move_tried, self, state)
         # remove from moves not tried of the node
         self._moves_not_tried.remove(move_tried)
         # add child
@@ -70,104 +80,32 @@ class MCTSNode:
 
         return node
 
-    def update(self, result):
-        """ update this node - one additional visit and result additional wins. result must be from the viewpoint of playerJustmoved.
-        """
-        self._wins = self._wins + result
-        self._visits = self._visits + 1
 
-    def __repr__(self):
-        out = "{Move - " + str(self._move) + " Wins/Visits:" + str(self._wins) + "/"\
-                + str(self._visits) + " UCT:" + str(self._moves_not_tried) + "}"
-        return out
+def agent_mcts(board, time_limit=1):
+    """ tic tac toe agent that takes in a tic tac toe board object and returns
+     the best move based on a Monte Carlo Tree Search agent. A time limit is set such
+     that the best move after time_limit many seconds is returned. """
 
-    # def tree_as_string(self, indent):
-    #     """
-    #     :param indent: level of indent
-    #     :return: returns a string representation of the tree
-    #     """
-    #     string_out = self.string_indent(indent) + str(self)
-    #     for child in self._children:
-    #         string_out += child.tree_as_string(indent + 1)
-    #     return string_out
-    #
-    # def string_indent(self, indent):
-    #     """
-    #     :param indent: level of indent
-    #     :return: returns indentation to represent the tree
-    #     """
-    #     string_out = "\n"
-    #     for _ in range(1, indent + 1):
-    #         string_out += "| "
-    #     return string_out
-    #
-    # def children_as_string(self):
-    #     string_out = ""
-    #     for child in self._children:
-    #         string_out += str(child) + "\n"
-    #     return string_out
-
-
-def uct(rootstate, time_limit, verbose=False):
-    """ We use the upper confidence bound for trees search method here. """
-
-    root = MCTSNode(state=rootstate)
-
-    # time limit for MCTS
-    timeout = time.time() + time_limit
-
-    while time.time() < timeout:
-        node = root
-        state = rootstate.clone()
-
-        # Step 1: Selection
-        while node.get_moves_not_tried() == [] and node.get_children() != []:  # node is fully expanded and non-terminal
-            node = node.choose_child_uct()
-            state.place_move(node.get_move())
-
-        # Step 2: Expansion
-        if node.get_moves_not_tried() != []:  # if we can expand (i.e. state/node is non-terminal)
-            m = random.choice(node.get_moves_not_tried())
-            state.place_move(m)
-            node = node.child_add(m, state)  # add child and descend tree
-
-        # Step 3: Simulation
-        state.rollout()
-
-        # Step 4: Back-propagate
-        while node is not None:  # backpropagate from the expanded node and work back to the root node
-            node.update(state.get_curr_players_result(
-                node.get_prev_player()))  # state is terminal. update node with result from POV of node._prev_player
-            node = node.get_parent()
-
-    # Output some information about the tree - can be omitted
-    # if verbose:
-    #     print(root.tree_as_string(0))
-    # else:
-    #     print(root.children_as_string())
-
-    return sorted(root.get_children(), key=lambda c: c.get_visits())[-1].get_move()  # return the move that was most visited
-
-
-def agent_mcts(board, time_limit):
-    """ We use the upper confidence bound for trees search method here. """
-
+    # setting board state as the MCTS root node
     root = MCTSNode(state=board)
 
     # time limit for MCTS
     timeout = time.time() + time_limit
 
-    while time.time() < timeout:
+    while time.time() < timeout:  # loop is broken if time limit for search is exceeded
+
         node = root
+        # make a copy of the board
         state = board.clone()
 
         # Step 1: Selection
-        while node.get_moves_not_tried() == [] and node.get_children() != []:  # node is fully expanded and non-terminal
-            node = node.choose_child_uct()
+        # whilst it is a not a terminal node and the node as no more expansions possible
+        while not node.get_moves_not_tried() and node.get_children():
+            node = node.choose_child_uct()  # select and set node based on UCT formula
             state.place_move(node.get_move())
 
         # Step 2: Expansion
-        if node.get_moves_not_tried() != []:  # if we can expand (i.e. state/node is non-terminal)
+        if not node.get_moves_not_tried():  # if we can expand (i.e. state/node is non-terminal)
             m = random.choice(node.get_moves_not_tried())
             state.place_move(m)
             node = node.child_add(m, state)  # add child and descend tree
@@ -181,10 +119,4 @@ def agent_mcts(board, time_limit):
                 node.get_prev_player()))  # state is terminal. update node with result from POV of node._prev_player
             node = node.get_parent()
 
-    # Output some information about the tree - can be omitted
-    # if verbose:
-    #     print(root.tree_as_string(0))
-    # else:
-    #     print(root.children_as_string())
-
-    return sorted(root.get_children(), key=lambda c: c.get_visits())[-1].get_move()  # return the move that was most visited
+    return sorted(root.get_children(), key=lambda child: child.get_visits())[-1].get_move()  # return the move that was most visited
